0aec685700ccd421a8c80cc478532fa2443ebfe5
James Njenga <njengajames232@gmail.com>
2024-09-03 23:42:00 +0300

    added the ability to scrape data in paginated format from page 1 to 10

--- scrape.js
+++ scrape.js
@@ -6,46 +6,49 @@ const scrape = async () => {
   const browser = await puppeteer.launch();
   // 
   const page = await browser.newPage();
+
+  const allBooks = [];
   
-  const url = "https://books.toscrape.com/";
-  await page.goto(url); // Go to the website
+  let currentPage = 1;
+  let maxPages = 10;
+
+  while (currentPage <= maxPages) {
+    const url = `https://books.toscrape.com/catalogue/page-${currentPage}.html`;
+    await page.goto(url); // Go to the website
 
 
-  // GET PAGE TITLE
-  // const result = await page.evaluate(() => {
-  //   return document.title;
-  // });
-  // console.log(result);
 
-  // ======0R ======
+    const books = await page.evaluate(() => {
+      const bookElements = document.querySelectorAll('.product_pod');
+      return Array.from(bookElements).map((book) => {
+        const title = book.querySelector('h3 a').getAttribute('title');
+        const price = book.querySelector('.price_color').innerText;
+        const stock = book.querySelector('.instock.availability') ? 'In Stock' : 'Out of Stock';
+        const rating = book.querySelector('.star-rating').className.split(' ')[1];
+        const link = book.querySelector('h3 a').href;
+
+        return {
+          title,
+          price,
+          stock,
+          rating,
+          link
+        };
+      })
+    });
+
+    allBooks.push(...books);
+    console.log(`Scraped page ${currentPage}`, books);
+    currentPage++;
+  }
+
   
-  // const title = await page.title();
-  // console.log("Page title is " + title);
+  
 
-
-  const books = await page.evaluate(() => {
-    const bookElements = document.querySelectorAll('.product_pod');
-    return Array.from(bookElements).map((book) => {
-      const title = book.querySelector('h3 a').getAttribute('title');
-      const price = book.querySelector('.price_color').innerText;
-      const stock = book.querySelector('.instock.availability') ? 'In Stock' : 'Out of Stock';
-      const rating = book.querySelector('.star-rating').className.split(' ')[1];
-      const link = book.querySelector('h3 a').href;
-
-      return {
-        title,
-        price,
-        stock,
-        rating,
-        link
-      };
-    })
-  });
-
-  console.log(books);
+  
 
   // Write the data to a json file
-  fs.writeFile('books.json', JSON.stringify(books, null, 2), (err) => {
+  fs.writeFile('books.json', JSON.stringify(allBooks, null, 2), (err) => {
     if (err) throw err;
     console.log('Data has been written to file');
   });
